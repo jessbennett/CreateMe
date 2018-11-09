a4cb610b5b5b3fa4cf35858d17d0a392f7046dad
stevenpearce2018 <stevenpearce2018@gmail.com>
2018-11-09 14:25:38 -0500

    push

--- client/src/components/ContactForm/contactform.js
+++ client/src/components/ContactForm/contactform.js
@@ -13,7 +13,7 @@ render() {
 return (
 <fieldset>
 <Label hasLabel={this.props.hasLabel} htmlFor={this.props.htmlFor} label={this.props.label} className={this.props.className} icon={this.props.icon} />
-<textarea cols={this.props.cols || ''} name={this.props.name || ''} required={this.props.required || ''} rows={this.props.rows || ''} value={this.props.textarea} onChange={this.props.onChange || ''} className={this.props.className}>
+<textarea cols={this.props.cols || ''} name={this.props.name || ''} required={ true || ''} rows={this.props.rows || ''} value={this.props.textarea} onChange={this.props.onChange || ''} className={this.props.className}>
         </textarea>
 </fieldset>
 );
