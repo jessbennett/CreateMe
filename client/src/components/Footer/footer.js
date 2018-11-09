11ff9467daf09a6780a1f17326225fd583f57260
stevenpearce2018 <stevenpearce2018@gmail.com>
2018-11-09 13:45:41 -0500

    Compressed

--- client/src/components/Footer/footer.js
+++ client/src/components/Footer/footer.js
@@ -1,24 +1,22 @@
 import React, { Component } from 'react';
 import './footer.css';
-
 class Footer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-    }
-  render() {
-    return (
-      <div>
-        <footer>
-            <ul className="social">
-                <li><a href="https://www.facebook.com/CreateMe.Software/" ><i className="icon-youtube-play"></i></a></li>
-                <li><a href="https://www.facebook.com/CreateMe.Software/" ><i className="icon-facebook"></i></a></li>
-            </ul>
-        </footer>
-      </div>
-    );
-  }
+constructor(props) {
+super(props);
+this.state = {
+};
 }
-
+render() {
+return (
+<div>
+<footer>
+<ul className="social">
+<li><a href="https://www.youtube.com/channel/UC9ceoc43SX4DJLiWKswPukg"><i className="icon-youtube-play"></i></a></li>
+<li><a href="https://www.facebook.com/CreateMe.Software/"><i className="icon-facebook"></i></a></li>
+</ul>
+</footer>
+</div>
+);
+}
+}
 export default Footer
--- client/src/components/Home/home.js
+++ client/src/components/Home/home.js
@@ -1,32 +1,26 @@
 import React, { Component } from 'react';
 import './home.css';
-
 class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+constructor(props) {
+super(props);
+this.state = {
 // No Data Avaible
-    };
-    }
-
-
-  render() {
-    return (
-<div>
- <div className="sp-container">
-  {/* <div className="sp-content"> */}
-	<h2 className="frame-1">Online Stores</h2>
-	<h2 className="frame-2">Mobile Apps</h2>
-	<h2 className="frame-3">Web Design</h2>
-	<h2 className="frame-4">Hosting</h2>
-	<h2 className="frame-5"><span>CreateMe</span></h2>
-  {/* </div> */}
- </div>
-</div>
-
-    
-    );
-  }
+};
 }
-
+render() {
+return (
+<div>
+<div className="sp-container">
+{/* <div className="sp-content"> */}
+<h2 className="frame-1">Online Stores</h2>
+<h2 className="frame-2">Mobile Apps</h2>
+<h2 className="frame-3">Web Design</h2>
+<h2 className="frame-4">Hosting</h2>
+<h2 className="frame-5"><span>CreateMe</span></h2>
+{/* </div> */}
+</div>
+</div>
+);
+}
+}
 export default Home
